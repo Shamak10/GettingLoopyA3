b759c808776d8553d4753f828a1010292a016e5d
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-02-24 12:49:33 -0500

    Update countingUpByThree.java

--- src/countingUpByThree.java
+++ src/countingUpByThree.java
@@ -1,5 +1,10 @@
-public class Main {
+public class countingUpByThree {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(""); // prints a blank line
+        for (int i = 0; i <= 18; i += 3) { // counts from 0 to 18 in increments of 3
+            {
+                System.out.println(i + " "); // prints the current count followed by a space
+            }
+        }
     }
 }
